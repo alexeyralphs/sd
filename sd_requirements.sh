11d651d9ab20ef2226e2a4630451d018f9698787
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 20:59:18 +0300

    Create sd_requirements.sh

---
+++ sd_requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BLUE_BG=$(tput setab 6)
+GREEN_BG=$(tput setab 42)
+BLACK_FG=$(tput setaf 0)
+RESET=$(tput sgr0)
+
+sd_requirements() {
+  sudo apt-get install -y python3.10 python3-pip python3-venv libgl1 libglib2.0-0 google-perftools git
+
+  sudo wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O /var/www/$admin_name/webui.sh
+  sudo mkdir -vp /var/www/$admin_name
+
+  sudo chmod +x /var/www/$admin_name/webui.sh
+
+  sudo -u sdadmin /var/www/$admin_name/webui.sh
+
+  echo '# For CPU render:' >  /var/www/$admin_name/stable-diffusion-webui/webui-user.sh
+  echo 'export COMMANDLINE_ARGS="--autolaunch --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --use-cpu all --no-half --enable-insecure-extension-access"' >>  /var/www/$admin_name/stable-diffusion-webui/webui-user.sh
+  echo '# For GPU render:' >>  /var/www/$admin_name/stable-diffusion-webui/webui-user.sh
+  echo '# export COMMANDLINE_ARGS="--autolaunch --update-check --no-half-vae --xformers --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access"' >>  /var/www/$admin_name/stable-diffusion-webui/webui-user.sh
+
+  wget https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/blob/main/v1-5-pruned-emaonly.safetensors -O /var/www/$admin_name/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
+
+  curl -s -o /usr/lib/systemd/system/sdwebui.service https://raw.githubusercontent.com/alexeyralphs/sd/refs/heads/main/sdwebui.service
+  sudo sed -i "s/\$admin_name/$admin_name/g" /usr/lib/systemd/system/sdwebui.service
+}

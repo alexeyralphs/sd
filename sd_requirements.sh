62e808065f584418be7e0f9aa131ea93bd569bf1
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 22:07:05 +0300

    Update sd_requirements.sh

--- sd_requirements.sh
+++ sd_requirements.sh
@@ -6,7 +6,13 @@ BLACK_FG=$(tput setaf 0)
 RESET=$(tput sgr0)
 
 sd_requirements() {
-  sudo apt-get install -y python3.10 python3-pip python3-venv libgl1 libglib2.0-0 google-perftools git
+  sudo apt -o Dpkg::Options::="--force-confold" install python3.10 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install python3-pip -y
+  sudo apt -o Dpkg::Options::="--force-confold" install python3-venv -y
+  sudo apt -o Dpkg::Options::="--force-confold" install libgl1 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install libglib2.0-0 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install google-perftools -y
+  sudo apt -o Dpkg::Options::="--force-confold" install git -y
 
   sudo mkdir -vp /var/www/$admin_name/stable-diffusion-webui
   sudo wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O /var/www/$admin_name/webui.sh

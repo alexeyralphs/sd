35388425ef5be9c8eb5f8df3f0bbfe23f4827b97
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 22:39:09 +0300

    Update sd_requirements.sh

--- sd_requirements.sh
+++ sd_requirements.sh
@@ -11,6 +11,7 @@ sd_requirements() {
   sudo apt -o Dpkg::Options::="--force-confold" install python3-venv -y
   sudo apt -o Dpkg::Options::="--force-confold" install libgl1 -y
   sudo apt -o Dpkg::Options::="--force-confold" install libglib2.0-0 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install bc -y
   sudo apt -o Dpkg::Options::="--force-confold" install google-perftools -y
   sudo apt -o Dpkg::Options::="--force-confold" install git -y
 

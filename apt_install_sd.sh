3b886ec939a4b72ad6fe50338df07e9d9cabe8e3
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 23:38:38 +0300

    Update apt_install_sd.sh

--- apt_install_sd.sh
+++ apt_install_sd.sh
@@ -6,9 +6,7 @@ BLACK_FG=$(tput setaf 0)
 RESET=$(tput sgr0)
 
 apt_install_sd() {
-  sudo apt -o Dpkg::Options::="--force-confold" install python3 -y
   sudo apt -o Dpkg::Options::="--force-confold" install python3.10 -y
-  sudo apt -o Dpkg::Options::="--force-confold" install python3.11 -y
   sudo apt -o Dpkg::Options::="--force-confold" install python3-pip -y
   sudo apt -o Dpkg::Options::="--force-confold" install python3-venv -y
   sudo apt -o Dpkg::Options::="--force-confold" install libgl1 -y

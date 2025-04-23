730bbd622b0bfa6cdf77b7396b41eb6135bff715
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-23 20:14:43 +0300

    Update apt_install_sd.sh

--- apt_install_sd.sh
+++ apt_install_sd.sh
@@ -9,6 +9,7 @@ apt_install_sd() {
   sudo apt-get install -y python3.10 python3-pip python3-venv libgl1 libglib2.0-0 google-perftools git
 
   sudo wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O /var/www/$admin_name/webui.sh
+  sudo mkdir -vp /var/www/$admin_name/stable-diffusion-webui
 
   sudo chmod +x /var/www/$admin_name/webui.sh
 

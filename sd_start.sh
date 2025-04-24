5fda59936a5508d346cb368675546fa57a11dec2
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-24 03:32:41 +0300

    Create sd_start.sh

---
+++ sd_start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BLUE_BG=$(tput setab 6)
+GREEN_BG=$(tput setab 42)
+BLACK_FG=$(tput setaf 0)
+RESET=$(tput sgr0)
+
+sd_start() {
+  sudo systemctl daemon-reload
+  sudo systemctl enable sdwebui
+  sudo systemctl start sdwebui
+  sudo systemctl restart sdwebui
+
+  git clone https://github.com/Zyin055/Config-Presets.git /var/www/$admin_name/stable-diffusion-webui/extensions/Config-Presets
+  curl -s -o /var/www/$admin_name/stable-diffusion-webui/extensions/Config-Presets/config-txt2img.json https://raw.githubusercontent.com/alexeyralphs/sd/refs/heads/main/config-txt2img.json
+  curl -s -o /var/www/$admin_name/stable-diffusion-webui/extensions/Config-Presets/config-img2img.json https://raw.githubusercontent.com/alexeyralphs/sd/refs/heads/main/config-img2img.json
+
+  git clone https://github.com/ArtVentureX/sd-webui-agent-scheduler.git /var/www/$admin_name/stable-diffusion-webui/extensions/sd-webui-agent-scheduler
+  git clone https://github.com/zanllp/sd-webui-infinite-image-browsing.git /var/www/$admin_name/stable-diffusion-webui/extensions/sd-webui-infinite-image-browsing
+
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 5
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 10
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 15
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 20
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 25
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 30
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+  sleep 35
+  sudo journalctl -u sdwebui -n 1 --no-pager -o cat
+  sleep 5
+  echo "${BLUE_BG}${BLACK_FG}Wait...${RESET}"
+
+  wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors -O /var/www/$admin_name/stable-diffusion-webui/models/Stable-diffusion/sd_xl_base_1.0.safetensors
+
+  sudo systemctl restart sdwebui
+}

d9a1990ef4254ab6b4da77922f7f103ddb1b8b2a
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-04-30 20:00:35 +0300

    Update apt_install_sd.sh

--- apt_install_sd.sh
+++ apt_install_sd.sh
@@ -10,6 +10,9 @@ apt_install_sd() {
   add-apt-repository ppa:deadsnakes/ppa -y
   apt update
   sudo apt -o Dpkg::Options::="--force-confold" install python3.10 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install python3.11 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install python3.12 -y
+  sudo apt -o Dpkg::Options::="--force-confold" install python3.13 -y
   sudo apt -o Dpkg::Options::="--force-confold" install python3-pip -y
   sudo apt -o Dpkg::Options::="--force-confold" install python3-venv -y
   sudo apt -o Dpkg::Options::="--force-confold" install libgl1 -y

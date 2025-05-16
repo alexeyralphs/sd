a8d5f5c2655a18a7abe24ec6f53f3f797eaa8877
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-05-17 00:21:12 +0300

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,5 +1,5 @@
 # For CPU render:
-export COMMANDLINE_ARGS="--autolaunch --gradio-auth $admin_name:$PASSWORD --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --use-cpu all"
+export COMMANDLINE_ARGS="--autolaunch --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --use-cpu all"
 # For GPU render:
-# export COMMANDLINE_ARGS="--autolaunch --gradio-auth $admin_name:$PASSWORD --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --xformers"
+# export COMMANDLINE_ARGS="--autolaunch --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --xformers"
 export LD_PRELOAD=libtcmalloc.so.4

80f182db502fd9e080716af4fe1dd6a685aeb110
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-05-16 21:08:27 +0300

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -2,3 +2,4 @@
 export COMMANDLINE_ARGS="--autolaunch --gradio-auth $admin_name:$PASSWORD --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --use-cpu all"
 # For GPU render:
 # export COMMANDLINE_ARGS="--autolaunch --gradio-auth $admin_name:$PASSWORD --api --api-auth $admin_name:$PASSWORD --upcast-sampling --update-check --no-half-vae --medvram-sdxl --opt-sdp-attention --skip-torch-cuda-test --no-half --enable-insecure-extension-access --xformers"
+export LD_PRELOAD=libtcmalloc.so.4

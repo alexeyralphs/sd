2aaf2759ded2f66f856743acc9c5eda56d4ea6b2
alexeyralphs <138135367+alexeyralphs@users.noreply.github.com>
2025-05-19 19:31:29 +0300

    Update webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -1,5 +1,5 @@
 # For CPU render:
 export COMMANDLINE_ARGS="--api --api-auth $admin_name:$PASSWORD --lowvram --opt-sdp-attention --enable-insecure-extension-access --upcast-sampling --update-check --no-half-vae --skip-torch-cuda-test --no-half --use-cpu all"
 # For GPU render:
-# export COMMANDLINE_ARGS="--api --api-auth $admin_name:$PASSWORD --lowvram --opt-sdp-attention --enable-insecure-extension-access --upcast-sampling --update-check --no-half-vae --skip-torch-cuda-test --no-half"
+# export COMMANDLINE_ARGS="--api --api-auth $admin_name:$PASSWORD --lowvram --opt-sdp-attention --enable-insecure-extension-access --upcast-sampling --update-check --no-half-vae --skip-torch-cuda-test"
 export LD_PRELOAD=libtcmalloc.so.4
